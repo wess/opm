33776974253cea12250bda0f357a34fa6173dfa3
wess <me@wess.io>
2024-02-14 13:18:43 -0500

    Fixes pathing issue

---
+++ libexec/libs/__curdir.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+define(){ IFS='\n' read -r -d '' ${1} || true; }
+
+curdir() {
+  SOURCE=${BASH_SOURCE[0]}
+  while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+    SOURCE=$(readlink "$SOURCE")
+    [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  done
+  
+  echo "$SOURCE"
+}
+
+
